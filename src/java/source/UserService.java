c9d444fa3d549784bbf3ebb35a56f988591fc6ce
Limyandi Vicotrico <limyandi@gmail.com>
2017-09-19 15:14:56 +1000

    Refactor code

--- src/java/source/UserService.java
+++ src/java/source/UserService.java
@@ -15,8 +15,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import source.User;
-import source.userApp;
 @Path("/user")
 public class UserService 
 {
