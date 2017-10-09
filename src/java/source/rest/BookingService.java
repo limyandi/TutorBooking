dabc068368dc4ddb4054d973b54bea552ee9e897
Limyandi Vicotrico <limyandi@gmail.com>
2017-10-09 21:42:47 +1100

    REST

--- src/java/source/rest/BookingService.java
+++ src/java/source/rest/BookingService.java
@@ -109,27 +109,27 @@ public class BookingService {
     @GET
     @Produces("text/xml")
     public Booking getBookingByID(@QueryParam("id") int id) throws Exception {
-        return getUserApp().getBookings().getBooking(id);
+        return getUserApp().getBookings().checkId(id);
     }
 
     @Path("emailsearch")
     @GET
     @Produces("text/xml")
     public Bookings getBookingByEmail(@QueryParam("email") String email) throws Exception {
-        return getUserApp().getBookings().getByEmail(email);
+        return getUserApp().getBookings().getByEmails(email);
     }
 
     @Path("subjectsearch")
     @GET
     @Produces("text/xml")
     public Bookings getBookingByName(@QueryParam("subject") String name) throws Exception {
-        return getUserApp().getBookings().getBySubject(name);
+        return getUserApp().getBookings().getBySubjects(name);
     }
 
     @Path("statussearch")
     @GET
     @Produces("text/xml")
     public Bookings getBookingByStatus(@QueryParam("status") String name) throws Exception {
-        return getUserApp().getBookings().getByStatus(name);
+        return getUserApp().getBookings().getByStatuses(name);
     }
 }
